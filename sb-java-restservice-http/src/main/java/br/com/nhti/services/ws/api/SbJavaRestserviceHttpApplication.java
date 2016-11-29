8310d180bb6b21fb072007e3bc2bf2c414581456
charlie <charlie@192.168.0.100>
2016-11-28 22:23:03 -0200

    Rest OK

---
+++ sb-java-restservice-http/src/main/java/br/com/nhti/services/ws/api/SbJavaRestserviceHttpApplication.java
@@ -0,0 +1,12 @@
+package br.com.nhti.services.ws.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJavaRestserviceHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJavaRestserviceHttpApplication.class, args);
+	}
+}
